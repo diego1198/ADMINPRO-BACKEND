5f170970331197031e3d78c94660503e89291957
Diego Beltrán <diegoandres1198@gmail.com>
2020-12-15 21:57:01 -0500

    Add Cors

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const app = express();
 app.use(cors());
 //Parse JSON
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
 //DB
 dbConnection();
 
